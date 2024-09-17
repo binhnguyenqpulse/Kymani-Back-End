50ff19e20ff15b038a979e097c6f10b30958c9ca
Binh Nguyen <binh.nguyen@ryerson.ca>
2024-09-17 15:09:09 -0400

    instructions

--- import_file.py
+++ import_file.py
@@ -7,10 +7,23 @@ Created on Tue Sep 17 14:51:47 2024
 """
 
 # Please do the following
+# - Create new branch 
+# git branch 'name of branch'
+# git checkout 'name of branch'
+# git pull
 # - create a import json file in the utils function
 # - import the json file here
 # - apply crud to it and adjust the functions accordingly in the utils functions
 # - test case of missing data (should we have missing data or should we not have missing data?)
+
+
+# Example
+# x = json(input)
+# create_user_account (x.....x....) but make sure this fits the schema of our code
+
+
+
+
 
 
 
